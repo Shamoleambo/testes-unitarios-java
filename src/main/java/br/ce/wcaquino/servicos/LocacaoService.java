7e63d7f6d59aac39fb46fbe7c53ea12f8b3bd8a4
Tiago Gomes <tidgomes@gmail.com>
2024-03-20 11:07:17 -0300

    create first test

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,12 +4,15 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 
 public class LocacaoService {
-	
+
 	public Locacao alugarFilme(Usuario usuario, Filme filme) {
 		Locacao locacao = new Locacao();
 		locacao.setFilme(filme);
@@ -17,18 +20,25 @@ public class LocacaoService {
 		locacao.setDataLocacao(new Date());
 		locacao.setValor(filme.getPrecoLocacao());
 
-		//Entrega no dia seguinte
+		// Entrega no dia seguinte
 		Date dataEntrega = new Date();
 		dataEntrega = adicionarDias(dataEntrega, 1);
 		locacao.setDataRetorno(dataEntrega);
-		
-		//Salvando a locacao...	
-		//TODO adicionar método para salvar
-		
+
+		// Salvando a locacao...
+		// TODO adicionar método para salvar
+
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Mano");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
 		
+		Locacao locacao = service.alugarFilme(usuario, filme);
+
+		Assert.assertTrue(locacao.getValor() == 5.0);
 	}
 }
